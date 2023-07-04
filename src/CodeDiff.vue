f9fcc9b2466cc4b07359ae7f6c3f1642fe3b493e
Oxrial <35791730+Oxrial@users.noreply.github.com>
2023-07-04 17:17:21 +0800

    添加主题导入api importTheme(function(){import('XXXX')} || null)，默认导入 theme monaco，支持highlight的主题包，例import('highlight.js/scss/monokai-sublime.scss')

--- src/CodeDiff.vue
+++ src/CodeDiff.vue
@@ -1,71 +1,86 @@
 <script setup lang="ts">
-import { computed, onMounted } from 'vue-demi'
-import { createSplitDiff, createUnifiedDiff } from './utils'
-import UnifiedViewer from './unified/UnifiedViewer.vue'
-import SplitViewer from './split/SplitViewer.vue'
+import { computed, onMounted } from "vue-demi";
+import { createSplitDiff, createUnifiedDiff } from "./utils";
+import UnifiedViewer from "./unified/UnifiedViewer.vue";
+import SplitViewer from "./split/SplitViewer.vue";
 
-import './style.scss'
+import "./style.scss";
 
 interface Props {
-  theme: string
-  newString: string
-  oldString: string
-  language?: string
-  context?: number
-  diffStyle?: 'word' | 'char'
-  outputFormat?: 'line-by-line' | 'side-by-side'
-  trim?: boolean
-  noDiffLineFeed?: boolean
-  maxHeight?: number
-  filename?: string
+  theme: string;
+  newString: string;
+  oldString: string;
+  language?: string;
+  context?: number;
+  diffStyle?: "word" | "char";
+  outputFormat?: "line-by-line" | "side-by-side";
+  trim?: boolean;
+  noDiffLineFeed?: boolean;
+  maxHeight?: number;
+  filename?: string;
 }
 
 const props = withDefaults(defineProps<Props>(), {
-  theme: '',
-  language: 'plaintext',
+  theme: "",
+  language: "plaintext",
   context: 10,
-  diffStyle: 'word',
-  outputFormat: 'line-by-line',
+  diffStyle: "word",
+  outputFormat: "line-by-line",
   trim: false,
   noDiffLineFeed: false,
   maxHeight: undefined,
   filename: undefined,
-})
-// onMounted(() => {
-//   switch (props.theme) {
-//     case 'vs':
-//       import('highlight.js/scss/vs.scss')
-//       break
-//     case 'monokai-sublime':
-//       import('highlight.js/scss/monokai-sublime.scss')
-//       break
-//     case 'synth-midnight-terminal-dark':
-//       import('highlight.js/scss/base16/synth-midnight-terminal-dark.scss')
-//       break
-//     case 'tomorrow-night':
-//       import('highlight.js/scss/base16/tomorrow-night.scss')
-//   }
-// })
-const importTheme = (func: Function): any => func()
-defineExpose({
-  importTheme,
-})
-const isUnifiedViewer = computed(() => props.outputFormat === 'line-by-line')
+});
+onMounted(() => {
+  importTheme();
+  //   switch (props.theme) {
+  //     case 'vs':
+  //       import('highlight.js/scss/vs.scss')
+  //       break
+  //     case 'monokai-sublime':
+  //       import('highlight.js/scss/monokai-sublime.scss')
+  //       break
+  //     case 'synth-midnight-terminal-dark':
+  //       import('highlight.js/scss/base16/synth-midnight-terminal-dark.scss')
+  //       break
+  //     case 'tomorrow-night':
+  //       import('highlight.js/scss/base16/tomorrow-night.scss')
+  //   }
+});
+const importTheme = (func: Function | null = null): any =>
+  (func && func()) ||
+  (function () {
+    import("./style/monaco-yaml.scss");
+  })();
+defineExpose({ importTheme });
+const isUnifiedViewer = computed(() => props.outputFormat === "line-by-line");
 
 const oldString = computed(() => {
-  const value = props.trim ? props.oldString.trim() : props.oldString
-  return props.noDiffLineFeed ? value.replace(/(\r\n)/g, '\n') : value
-})
+  const value = props.trim ? props.oldString.trim() : props.oldString;
+  return props.noDiffLineFeed ? value.replace(/(\r\n)/g, "\n") : value;
+});
 const newString = computed(() => {
-  const value = props.trim ? props.newString.trim() : props.newString
-  return props.noDiffLineFeed ? value.replace(/(\r\n)/g, '\n') : value
-})
+  const value = props.trim ? props.newString.trim() : props.newString;
+  return props.noDiffLineFeed ? value.replace(/(\r\n)/g, "\n") : value;
+});
 
 const diffChange = computed(() =>
   isUnifiedViewer.value
-    ? createUnifiedDiff(oldString.value, newString.value, props.language, props.diffStyle, props.context)
-    : createSplitDiff(oldString.value, newString.value, props.language, props.diffStyle, props.context),
-)
+    ? createUnifiedDiff(
+        oldString.value,
+        newString.value,
+        props.language,
+        props.diffStyle,
+        props.context
+      )
+    : createSplitDiff(
+        oldString.value,
+        newString.value,
+        props.language,
+        props.diffStyle,
+        props.context
+      )
+);
 </script>
 
 <template>
@@ -74,8 +89,12 @@ const diffChange = computed(() =>
       <div class="file-info">
         <span class="filename">{{ filename }}</span>
         <span class="diff-stat">
-          <span class="diff-stat-added">+{{ diffChange.stat.additionsNum }} additions</span>
-          <span class="diff-stat-deleted" style="margin-left: 8px;">-{{ diffChange.stat.deletionsNum }} deletions</span>
+          <span class="diff-stat-added"
+            >+{{ diffChange.stat.additionsNum }} additions</span
+          >
+          <span class="diff-stat-deleted" style="margin-left: 8px"
+            >-{{ diffChange.stat.deletionsNum }} deletions</span
+          >
         </span>
       </div>
     </div>
--- vue2-playground/App.vue
+++ vue2-playground/App.vue
@@ -1,36 +1,40 @@
 <script setup lang="ts">
-import { onMounted, reactive, ref, version } from 'vue-demi'
-import hljs from 'highlight.js'
+import { onMounted, reactive, ref, version } from "vue-demi";
+import hljs from "highlight.js";
 
 const form = reactive({
-  oldString: 'a:\n b: c',
-  newString: 'a:\n bc: \n c: d',
-  language: 'yaml',
-  diffStyle: 'word',
-})
-const code = ref(null)
-const t = () => {
-  import('highlight.js/scss/base16/tomorrow-night.scss')
-}
-onMounted(() => {
-  code.value && code.value.importTheme(t)
-})
+  oldString: "a:\n b: c",
+  newString: "a:\n bc: \n c: d",
+  language: "yaml",
+  diffStyle: "word",
+});
+const code = ref(null);
+onMounted(() => {});
 </script>
 
 <template>
   <div>
-    <p align="center">
-      Vue version: {{ version }}
-    </p>
+    <p align="center">Vue version: {{ version }}</p>
     <textarea v-model="form.oldString" style="width: 48vw" :rows="20" />
-    <textarea v-model="form.newString" style="width: 48vw; margin-left: 20px" :rows="20" />
-    <CodeDiff ref="code" style="background: #161616" :old-string="form.oldString" theme="tomorrow-night"
-      :new-string="form.newString" :language="form.language" :diff-style="form.diffStyle" output-format="side-by-side" />
+    <textarea
+      v-model="form.newString"
+      style="width: 48vw; margin-left: 20px"
+      :rows="20"
+    />
+    <CodeDiff
+      ref="code"
+      :old-string="form.oldString"
+      theme="tomorrow-night"
+      :new-string="form.newString"
+      :language="form.language"
+      :diff-style="form.diffStyle"
+      output-format="side-by-side"
+    />
   </div>
 </template>
 
 <style>
-body>div {
+body > div {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
